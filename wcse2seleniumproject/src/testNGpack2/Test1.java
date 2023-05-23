59b996cb8f4813c88c432773d9aef2034cceaec1
dell <dell@DESKTOP-81GRUB6>
2023-05-23 09:55:15 +0530

    commit

---
+++ wcse2seleniumproject/src/testNGpack2/Test1.java
@@ -0,0 +1,30 @@
+package testNGpack2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void test1Method() {
+	  
+	  System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+
+		EdgeOptions eo = new EdgeOptions();
+		eo.addArguments("--remote-allow-origins=*");
+
+		WebDriver driver= new EdgeDriver(eo);
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.get("https://www.google.com");
+		
+		driver.switchTo().activeElement().sendKeys("Java",Keys.ENTER);
+		
+  }
+}
